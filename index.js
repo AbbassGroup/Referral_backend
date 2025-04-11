914a172c623ce835c3d1f0014c5ab71239cad7c2
Abbass Group <webproject@abbass.group>
2025-04-11 19:39:26 +1000

    18th commit

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://abbass.group',
+    'https://portal-qz8y.onrender.com',
     'http://localhost:3001',
     'http://localhost:3000'
   ],
