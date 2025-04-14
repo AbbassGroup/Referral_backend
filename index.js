d357f0a1a675fbe626114188ef7441fbbd8627db
Abbass Group <webproject@abbass.group>
2025-04-14 22:24:09 +1000

    27th commit

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const handleLogin = async (req, res) => {
     }
 
     // 2. Check Partner (password is hashed)
-    const partner = await Partner.findOne({ username: name }).select('+password');
+    const partner = await Partner.findOne({ name: name }).select('+password');
     if (!partner) {
       return res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
@@ -626,7 +626,7 @@ app.post('/api/reset-password', async (req, res) => {
     const { name, password } = req.body;
 
     // Find the partner by username
-    const partner = await Partner.findOne({ username: name }).select('+password');
+    const partner = await Partner.findOne({ name: name }).select('+password');
     if (!partner) {
       return res.status(404).json({ message: 'User not found' });
     }
